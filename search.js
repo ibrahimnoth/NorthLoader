d2b537658b7c2b98f7364f4b0589f81827e54da0
ibrahimnoth <rkn50092@gmail.com>
2026-07-02 08:15:42 +0300

    Update search.js

--- search.js
+++ search.js
@@ -1,1 +1,22 @@
+const search = document.getElementById("search");
 
+if (search) {
+    search.addEventListener("input", function () {
+
+        const value = this.value.toLowerCase();
+        const cards = document.querySelectorAll(".card");
+
+        cards.forEach(card => {
+
+            const title = card.querySelector(".title").textContent.toLowerCase();
+
+            if (title.includes(value)) {
+                card.style.display = "block";
+            } else {
+                card.style.display = "none";
+            }
+
+        });
+
+    });
+}
