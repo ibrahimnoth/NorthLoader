51e84cf26263bf54e0ec4a9a6dcee478f2ee1e1e
ibrahimnoth <rkn50092@gmail.com>
2026-07-02 08:16:22 +0300

    Implement video popup functionality

--- popup.js
+++ popup.js
@@ -1,1 +1,27 @@
+const popup = document.getElementById("popup");
 
+function openVideo(url){
+
+document.getElementById("videoFrame").src=url;
+
+popup.style.display="flex";
+
+}
+
+function closeVideo(){
+
+popup.style.display="none";
+
+document.getElementById("videoFrame").src="";
+
+}
+
+window.onclick=function(e){
+
+if(e.target===popup){
+
+closeVideo();
+
+}
+
+}
